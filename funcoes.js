bc537ef54b7f6ac1bd55f2cc16d099a3d30d845a
PedroNoMatriz <00001080853017sp@al.educacao.sp.gov.br>
2022-03-28 18:10:48 -0300

    site finalizado
    ...............

---
+++ funcoes.js
@@ -0,0 +1,15 @@
+function PlayPause(){
+   let imagem = document.querySelector("#controle-musica")
+  let musica = document.querySelector("#musica")
+
+  if(musica.paused){
+      musica.play()
+          imagem.src="imagens/pause.png"
+          document.querySelector("rotulo").innerText = "clique aqui para pausar"
+
+  }else{
+      musica.pause()
+      imagem.src="imagens/play.png"
+      document.querySelector("#rotulo").innerText = "clique aqui para ouvir"
+  }
+}
